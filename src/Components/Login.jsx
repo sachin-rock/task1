193a8f90c076f95fc2ca3041330fbee15615a8a9
sacgin ingole <97524423+sachin-rock@users.noreply.github.com>
2022-10-26 13:33:10 +0530

    done

---
+++ src/Components/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Login.css";
+import img from "../Images/img.png";
+import back from "../Images/back.svg"
+
+
+
+const Login = () => {
+  return (
+    <div>
+      <div className="container">
+      <div  className="firstimg"> <img src={back} alt="" /> </div>
+        <img src={img} alt="" />
+        <section>Personal Account</section>
+        <h3>Andrew@polka.io</h3>
+        <label>Password</label><br />
+        <input className="input" type="password" placeholder="andrew@polka" />
+        <p>Forgot password?<a href=""><b>Reset</b></a></p><br />
+        <input type = "checkbox" /> Stay signed in  <button className="btn">Submit</button>
+        <hr />
+        <section> This site is protected by reCAPTCHA and the Google</section>
+       <p>Privacy Policy and Terms of Service apply</p>
+
+
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Login;
