e5309b2ece08647e7f7a6e5a77b18a1af7ecec01
sacgin ingole <97524423+sachin-rock@users.noreply.github.com>
2022-10-26 14:08:04 +0530

    done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Login.css";
 import img from "../Images/img.png";
-import back from "../Images/back.svg"
+
 
 
 
@@ -9,21 +9,17 @@ const Login = () => {
   return (
     <div>
       <div className="container">
-      <div  className="firstimg"> <img src={back} alt="" /> </div>
         <img src={img} alt="" />
         <section>Personal Account</section>
         <h3>Andrew@polka.io</h3>
         <label>Password</label><br />
-        <input className="input" type="password" placeholder="andrew@polka" />
+        <input className="input" type="password"  placeholder="andrew@polka" />
         <p>Forgot password?<a href=""><b>Reset</b></a></p><br />
         <input type = "checkbox" /> Stay signed in  <button className="btn">Submit</button>
         <hr />
         <section> This site is protected by reCAPTCHA and the Google</section>
        <p>Privacy Policy and Terms of Service apply</p>
 
-
-
-        
       </div>
     </div>
   );
